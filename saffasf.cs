527d4321235d9409151e8d2926d52235ec0ba64e
erersfsgghfh <153913795+erersfsgghfh@users.noreply.github.com>
2023-12-27 16:19:16 +0500

    Add files via upload

---
+++ saffasf.cs
@@ -0,0 +1,100 @@
+﻿/*
+int y;
+int x;
+int t = 0;
+do
+{
+    
+    x = Convert.ToInt32(Console.ReadLine());
+    x = Convert.ToInt32(Console.ReadLine());
+    t = t + x;
+
+
+
+
+} while 
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int t = 1;
+for (int i = 1; i <= y; i=i+1)
+{
+    
+    t = t * x;
+}
+Console.WriteLine(t);
+
+
+int x;
+int t = 0;
+do
+{
+    Console.WriteLine("Ведите число");
+    x = Convert.ToInt32(Console.ReadLine());
+    t = t + x;
+} while (x != 0);
+
+Console.WriteLine($"сума ровна {t} ");
+
+
+int x = 1;
+int sum = 0;
+while (x != 0)
+{
+    Console.WriteLine("Ведите число");
+    x = Convert.ToInt32(Console.ReadLine());
+    sum = sum + x;
+}
+
+Console.WriteLine($"сума ровна {sum} ");
+
+int x = 1;
+int sum = 0;
+int i = 0;
+do
+{
+    Console.WriteLine("Ведите число");
+    i ++;
+    x = Convert.ToInt32(Console.ReadLine());
+    sum = sum + x;
+} while (sum < 100);
+
+Console.WriteLine($"сума ровна {sum} ");
+Console.WriteLine($"веденно {i}  чисел");
+
+Console.WriteLine("Ведите пароль");
+string x = Console.ReadLine();
+string y;
+
+                                                                                                                                             
+int x;
+x = Convert.ToInt32(Console.ReadLine());
+int [] nums = new int [x];
+
+for (int i = 0; i < x; i = i + 1)
+    nums[i] = Convert.ToInt32(Console.ReadLine());
+
+int t = 0;
+   x = Convert.ToInt32(Console.ReadLine());
+    t = t + x;
+
+Console.WriteLine("Скоко вам лет?");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x >= 18)
+{
+    Console.WriteLine("вы совершеннолетний");
+}
+else if (x < 18)
+{
+    Console.WriteLine("вы не совершеннолетний");
+}
+
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int t = 1;
+for (int i = 1; i <= y; i = i + 1)
+{
+    
+    t = t + i;
+
+Console.WriteLine(y);
+*/
